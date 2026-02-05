68a4a6e81f31556cc6e6d989aa672c1172f678c4
Shivraj-Ekad <2024eb03301@online.bits-pilani.ac.in>
2026-02-05 19:35:03 +0530

    Add files via upload

---
+++ Question1/created_files/analyze.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+if [ $# -ne 1 ]; then
+    echo "Error: Provide exactly one argument."
+    exit 1
+fi
+
+path=$1
+
+if [ ! -e "$path" ]; then
+    echo "Error: Path does not exist."
+    exit 1
+fi
+
+if [ -f "$path" ]; then
+    echo "File Analysis:"
+    wc "$path"
+
+elif [ -d "$path" ]; then
+    echo "Directory Analysis:"
+    total_files=$(find "$path" -type f | wc -l)
+    txt_files=$(find "$path" -type f -name "*.txt" | wc -l)
+
+    echo "Total files: $total_files"
+    echo ".txt files: $txt_files"
+else
+    echo "Unknown type."
+fi
